678f707f345eaa3ce890066e807cd0794fe47bb7
mayank5654 <mayankkaushal99@gmail.com>
2023-11-27 15:59:10 +0530

    intial commit

---
+++ part1/part1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part1
+
+// Step 1: Create a delegate named ArithmeticOperation
+{ delegate int ArithmeticOperation(int a, int b);
+
+class Program
+{
+    // Step 2: Implement four static methods for arithmetic operations
+    static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    static int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+
+    static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    static int Divide(int a, int b)
+    {
+        // Check for division by zero
+        if (b != 0)
+        {
+            return a / b;
+        }
+        else
+        {
+            Console.WriteLine("Cannot divide by zero.");
+            return 0;
+        }
+    }
+
+    static void Main()
+    {
+        // Step 3: Create instances of the ArithmeticOperation delegate
+        ArithmeticOperation addDelegate = new ArithmeticOperation(Add);
+        ArithmeticOperation subtractDelegate = new ArithmeticOperation(Subtract);
+        ArithmeticOperation multiplyDelegate = new ArithmeticOperation(Multiply);
+        ArithmeticOperation divideDelegate = new ArithmeticOperation(Divide);
+
+        // Step 4: Ask the user to input two integers
+        Console.Write("Enter the first integer: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the second integer: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        // Step 5: Prompt the user to choose an arithmetic operation
+        Console.WriteLine("Choose an arithmetic operation:");
+        Console.WriteLine("1. Addition");
+        Console.WriteLine("2. Subtraction");
+        Console.WriteLine("3. Multiplication");
+        Console.WriteLine("4. Division");
+
+        Console.Write("Enter your choice (1-4): ");
+        int choice = Convert.ToInt32(Console.ReadLine());
+
+        // Step 6: Call the corresponding method through the delegate and display the result
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine($"Result of Addition: {addDelegate(num1, num2)}");
+                break;
+            case 2:
+                Console.WriteLine($"Result of Subtraction: {subtractDelegate(num1, num2)}");
+                break;
+            case 3:
+                Console.WriteLine($"Result of Multiplication: {multiplyDelegate(num1, num2)}");
+                break;
+            case 4:
+                Console.WriteLine($"Result of Division: {divideDelegate(num1, num2)}");
+                break;
+            default:
+                Console.WriteLine("Invalid choice.");
+                break;
+        }
+    }
+}
+}
---
+++ part2/part2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part2
+{
+
+    class Program
+    {
+        // Generic delegate
+        public delegate T Operation<T>(T x, T y);
+
+        // Generic static methods
+        public static T Add<T>(T x, T y) => (dynamic)x + y;
+        public static T Subtract<T>(T x, T y) => (dynamic)x - y;
+        public static T Multiply<T>(T x, T y) => (dynamic)x * y;
+        public static T Divide<T>(T x, T y) => (dynamic)x / y;
+
+        static void Main()
+        {
+            // Create instances of Operation<T> delegate for each operation
+            Operation<int> addDelegate = Add;
+            Operation<int> subtractDelegate = Subtract;
+            Operation<int> multiplyDelegate = Multiply;
+            Operation<int> divideDelegate = Divide;
+
+            // Ask the user to input two values of the same data type
+            Console.Write("Enter the first value: ");
+            int value1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the second value: ");
+            int value2 = Convert.ToInt32(Console.ReadLine());
+
+            // Prompt the user to choose an operation
+            Console.WriteLine("Choose an operation:");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+
+            // Get user's choice
+            Console.Write("Enter your choice (1-4): ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            // Perform the selected operation and display the result
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine($"Addition result: {addDelegate(value1, value2)}");
+                    break;
+                case 2:
+                    Console.WriteLine($"Subtraction result: {subtractDelegate(value1, value2)}");
+                    break;
+                case 3:
+                    Console.WriteLine($"Multiplication result: {multiplyDelegate(value1, value2)}");
+                    break;
+                case 4:
+                    // Handle division by zero separately
+                    if (value2 != 0)
+                    {
+                        Console.WriteLine($"Division result: {divideDelegate(value1, value2)}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Cannot divide by zero.");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice.");
+                    break;
+            }
+        }
+    }
+}
